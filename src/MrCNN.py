030ebe00f7e29942deacb027ce697bef0dd180f9
Nate <1505703611@qq.com>
2024-11-09 19:01:23 +0000

    feat: add l2-norm and dropout layers

--- src/MrCNN.py
+++ src/MrCNN.py
@@ -6,14 +6,24 @@ class MrCNN(nn.Module):
     def __init__(self):
         super().__init__()
         ## Convolutional layers for each branch
-        self.conv1 = nn.Conv2d(3, 96, kernel_size=7, stride=1, padding=0)
-        self.initialise_layer(self.conv1)
-        self.conv2 = nn.Conv2d(96, 160, kernel_size=3, stride=1, padding=0)
-        self.initialise_layer(self.conv2)
-        self.conv3 = nn.Conv2d(160, 288, kernel_size=3, stride=1, padding=0)
+        self.branch1_conv1 = nn.Conv2d(3, 96, kernel_size=7, stride=1, padding=0)
+        self.branch1_conv2 = nn.Conv2d(96, 160, kernel_size=3, stride=1, padding=0)
+        self.branch1_conv3 = nn.Conv2d(160, 288, kernel_size=3, stride=1, padding=0)
+
+        self.branch2_conv1 = nn.Conv2d(3, 96, kernel_size=7, stride=1, padding=0)
+        self.branch2_conv2 = nn.Conv2d(96, 160, kernel_size=3, stride=1, padding=0)
+        self.branch2_conv3 = nn.Conv2d(160, 288, kernel_size=3, stride=1, padding=0)
+
+        self.branch3_conv1 = nn.Conv2d(3, 96, kernel_size=7, stride=1, padding=0)
+        self.branch3_conv2 = nn.Conv2d(96, 160, kernel_size=3, stride=1, padding=0)
+        self.branch3_conv3 = nn.Conv2d(160, 288, kernel_size=3, stride=1, padding=0)
 
         ## layers for all branches
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # the paper uses a dropout layer with a rate of 0.5
+        # TODO: add this argument to parser to make it configurable
+        self.droupout = nn.Dropout(0.5)
 
         self.branch_fc = nn.Linear(2592, 512)
 
@@ -22,31 +32,35 @@ class MrCNN(nn.Module):
         self.output = nn.Linear(512, 1)
 
     def forward_branch(self, x, conv1, conv2, conv3):
+        # # Apply L2 norm constraint to the first convolutional layer
+        # # reference: paper section 3.2
+        with torch.no_grad():
+            norm = conv1.weight.norm(2, dim=(1, 2, 3), keepdim=True)
+            desired_norm = torch.clamp(norm, max=0.1)
+            conv1.weight *= desired_norm / (norm + 1e-8)
+
         x = F.relu(conv1(x))
-        # print(f'Shape after conv1: {x.shape}')
         x = self.pool(x)
-        # print(f'Shape after pool1: {x.shape}')
         x = F.relu(conv2(x))
-        # print(f'Shape after conv2: {x.shape}')
         x = self.pool(x)
-        # print(f'Shape after pool2: {x.shape}')
         x = F.relu(conv3(x))
-        # print(f'Shape after conv3: {x.shape}')
+        x = self.droupout(x)
         x = self.pool(x)
-        # print(f'Shape after pool3: {x.shape}')
         x = x.view(x.size(0), -1)
-        # print(f'Shape after flatten: {x.shape}')
-        x = F.sigmoid(self.branch_fc(x))
+        x = F.relu(self.branch_fc(x))
+        # add dropout layer after the FC layer
+        x = self.droupout(x)
         return x
 
     def forward(self, input1, input2, input3):
-        branch1_output = self.forward_branch(input1, self.conv1, self.conv2, self.conv3)
-        branch2_output = self.forward_branch(input2, self.conv1, self.conv2, self.conv3)
-        branch3_output = self.forward_branch(input3, self.conv1, self.conv2, self.conv3)
+        branch1_output = self.forward_branch(input1, self.branch1_conv1, self.branch1_conv2, self.branch1_conv3)
+        branch2_output = self.forward_branch(input2, self.branch2_conv1, self.branch2_conv2, self.branch2_conv3)
+        branch3_output = self.forward_branch(input3, self.branch3_conv1, self.branch3_conv2, self.branch3_conv3)
 
         combined = torch.cat((branch1_output, branch2_output, branch3_output), dim=1)
 
         combined = F.relu(self.fc_combined(combined))
+        combined = self.droupout(combined)
 
         output = torch.sigmoid(self.output(combined))
         return output
